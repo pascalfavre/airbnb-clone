cf67d8599ee2aef7f0facdf60453571438e9c252
Pascal Favre <pascal.favre@hevs.ch>
2020-09-29 09:14:23 +0200

    Add header and banner

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+import Banner from './components/Banner/Banner';
+
+function Home() {
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Search.css';
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+import { useHistory } from "react-router-dom";
+
+function Search() {
+  return (
+    <div className="search">
+      
+    </div>
+  )
+}
+
+export default Search
